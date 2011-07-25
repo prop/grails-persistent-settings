bbe20784471883b40f4953891045758b5d075dbf
prop <r.belyakov@netris.ru>
2011-07-25 14:20:16 +0400

    Version bump

--- PersistentSettingsGrailsPlugin.groovy
+++ PersistentSettingsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class PersistentSettingsGrailsPlugin {
     // the plugin version
-    def version = "0.5"
+    def version = "0.5.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
