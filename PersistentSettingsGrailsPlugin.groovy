490151d8829d0fa7bc83ea396a30668c895d821f
Alexey Nevinsky <a.nevinsky@netris.ru>
2014-09-09 17:42:51 +0400

    set version to 0.9.1

--- PersistentSettingsGrailsPlugin.groovy
+++ PersistentSettingsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class PersistentSettingsGrailsPlugin {
     // the plugin version
-    def version = "0.9.2-SNAPSHOT"
+    def version = "0.9.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
